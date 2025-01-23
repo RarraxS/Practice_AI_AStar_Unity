b7e451409e6216a2c700731f90b0495d25eca3e0
AleZamoraJu <alezamoraju@gmail.com>
2025-01-23 13:03:08 +0100

    Primeros pasos

--- Assets/Scripts/SampleMind/DepthSearchMind.cs
+++ Assets/Scripts/SampleMind/DepthSearchMind.cs
@@ -5,13 +5,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DepthSearchMind : AbstractPathMind
-{    public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+{
+    class Node
     {
+        public CellInfo info;
+        public Node parent;
 
-        var val = Random.Range(0, 4);
-        if (val == 0) return Locomotion.MoveDirection.Up;
-        if (val == 1) return Locomotion.MoveDirection.Down;
-        if (val == 2) return Locomotion.MoveDirection.Left;
-        return Locomotion.MoveDirection.Right;
+        public Node (CellInfo _info, Node _parent)
+        {
+            this.info = _info;
+            this.parent = _parent;
+        }
+    }
+
+    List<Node> VisitedNodes = new List<Node>();
+    List<Node> DiscoveredNodes = new List<Node>();
+    List<Node> PathNodes = new List<Node>();
+
+    public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
+    {
+        CellInfo goal = goals[0];
+        Node startingPoint = new Node(currentPos, null);
+
+        CellInfo finishingPoint = DFS(boardInfo, startingPoint, goal);
+
+        if (finishingPoint.RowId < startingPoint.info.RowId)
+            return Locomotion.MoveDirection.Left;
+        else if (finishingPoint.RowId > startingPoint.info.RowId)
+            return Locomotion.MoveDirection.Right;
+        else if (finishingPoint.ColumnId < startingPoint.info.ColumnId)
+            return Locomotion.MoveDirection.Down;
+
+        return Locomotion.MoveDirection.Up;
+    }
+
+    private CellInfo DFS(BoardInfo boardInfo, Node cell, CellInfo goal)
+    {
+        //Marco U como descubierto.
+        DiscoveredNodes.Add(cell);
+        //Por cada vértcie v adyacente a U.
+        foreach(CellInfo possibleAdjacentCell in boardInfo.CellInfos)
+        {
+            if ((possibleAdjacentCell.RowId == cell.info.RowId) && (possibleAdjacentCell.ColumnId == cell.info.ColumnId - 1) ||
+                (possibleAdjacentCell.RowId == cell.info.RowId) && (possibleAdjacentCell.ColumnId == cell.info.ColumnId + 1) ||
+                (possibleAdjacentCell.RowId == cell.info.RowId - 1) && (possibleAdjacentCell.ColumnId == cell.info.ColumnId) ||
+                (possibleAdjacentCell.RowId == cell.info.RowId + 1) && (possibleAdjacentCell.ColumnId == cell.info.ColumnId))
+            {
+                //if(v no fue visitado)
+                //{
+                // padre [v] = u;
+                // DFS(g,v);
+                //}
+            }
+        }
+
+        //
+        return cell.info;
     }
 }
