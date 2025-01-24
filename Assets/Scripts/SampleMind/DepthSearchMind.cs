181db1b271d11ddd83f4063f3f46c4f7c268c3d2
AleZamoraJu <alezamoraju@gmail.com>
2025-01-24 18:07:46 +0100

    moar cambios

--- Assets/Scripts/SampleMind/DepthSearchMind.cs
+++ Assets/Scripts/SampleMind/DepthSearchMind.cs
@@ -1,9 +1,8 @@
 using Assets.Scripts.DataStructures;
 using Assets.Scripts;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
-using Unity.VisualScripting;
+using System.Diagnostics;
 
 public class DepthSearchMind : AbstractPathMind
 {
@@ -12,7 +11,7 @@ public class DepthSearchMind : AbstractPathMind
         public CellInfo info;
         public Node parent;
 
-        public Node (CellInfo _info, Node _parent)
+        public Node(CellInfo _info, Node _parent)
         {
             this.info = _info;
             this.parent = _parent;
@@ -21,20 +20,26 @@ public class DepthSearchMind : AbstractPathMind
 
     bool foundGoal = false;
     List<Node> VisitedNodes = new List<Node>();
+    Node priorNode = null;
 
     public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
     {
-       
+
         CellInfo goal = goals[0];
         Node startingPoint = new Node(currentPos, null);
+
         VisitedNodes.Clear();
         foundGoal = false;
 
         if (!goal.Walkable)
             return Locomotion.MoveDirection.None;
 
-            CellInfo finishingPoint = DFS(boardInfo, startingPoint, goal);
-
+        CellInfo finishingPoint = DFS(boardInfo, startingPoint, goal);
+        if (finishingPoint.CellId == "6,14")
+        {
+            UnityEngine.Debug.Log("Prior:" + priorNode.info.CellId);
+            UnityEngine.Debug.Log("Post: " + finishingPoint.CellId);
+        }
         if (finishingPoint.RowId < startingPoint.info.RowId)
             return Locomotion.MoveDirection.Down;
         else if (finishingPoint.RowId > startingPoint.info.RowId)
@@ -52,30 +57,34 @@ public class DepthSearchMind : AbstractPathMind
 
         //Por cada vértce v adyacente a U.
         CellInfo[] neighbours = initNode.info.WalkableNeighbours(boardInfo);
-        Node[] adjacentNodes = new Node[neighbours.Length];     ;
-        int i=0;
-        foreach(CellInfo neighbour in neighbours) {
-            if (neighbours[i]!= null){
+        Node[] adjacentNodes = new Node[neighbours.Length]; ;
+        int i = 0;
+        foreach (CellInfo neighbour in neighbours){
+            if (neighbours[i] != null){
                 adjacentNodes[i] = new Node(neighbours[i], initNode);
             }
             i++;
         }
-        foreach(Node adjNode in adjacentNodes)
-        {
-            if(adjNode != null && adjNode.info == goal){
+        foreach (Node adjNode in adjacentNodes){
+            if (adjNode != null && adjNode.info == goal){
                 VisitedNodes.Add(adjNode);
                 foundGoal = true;
             }
-            else if(foundGoal){
+            else if (foundGoal){
                 VisitedNodes.Add(initNode);
                 break;
             }
-            else if (!VisitedNodes.Contains(adjNode) && adjNode != null){
-                DFS(boardInfo, adjNode, goal);
+                  //(!VisitedNodes.Contains(adjNode)
+            else if (adjNode != null){
+                if ((priorNode == null) || (!VisitedNodes.Any(node => node.info.CellId == adjNode.info.CellId) && (adjNode.info.CellId != priorNode.info.CellId))){
+                    
+                    priorNode = initNode;
+                    DFS(boardInfo, adjNode, goal);
+                }
             }
         }
-        if (VisitedNodes.Count>2) 
-            return VisitedNodes[VisitedNodes.Count-2].info;
+        if (VisitedNodes.Count > 2)
+            return VisitedNodes[VisitedNodes.Count - 2].info;
         else return null;
     }
 }
