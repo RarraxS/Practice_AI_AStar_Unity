1c049ef94e2d37bcc644ff336874f32856ecda24
AleZamoraJu <alezamoraju@gmail.com>
2025-01-26 23:47:35 +0100

    Cambios

--- Assets/Scripts/SampleMind/AStarMind.cs
+++ Assets/Scripts/SampleMind/AStarMind.cs
@@ -1,6 +1,5 @@
 using Assets.Scripts.DataStructures;
 using Assets.Scripts;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,80 +8,106 @@ public class AStarMind : AbstractPathMind
 {
     class Node
     {
-        public CellInfo info;
-        public Node parent;
-        public bool visited;
-        public Node(CellInfo _info, Node _parent, bool _visited)
+        public CellInfo Info;
+        public Node Parent;
+        public int GCost; // Cost from start to this node
+        public int HCost; // Heuristic cost (distance to goal)
+        public int FCost => GCost + HCost; // Total cost
+
+        public Node(CellInfo info, Node parent, int gCost, int hCost)
         {
-            this.info = _info;
-            this.parent = _parent;
-            this.visited = _visited;
+            Info = info;
+            Parent = parent;
+            GCost = gCost;
+            HCost = hCost;
         }
     }
 
-    bool foundGoal = false;
-    List<Node> PathNodes = new List<Node>();
-    int countNodes = 2;
-
     public override Locomotion.MoveDirection GetNextMove(BoardInfo boardInfo, CellInfo currentPos, CellInfo[] goals)
     {
-        CellInfo finishingPoint = new CellInfo(1, 0);
-        foundGoal = false;
+        if (goals == null || goals.Length == 0) return Locomotion.MoveDirection.None;
 
-        if (PathNodes.Count == 0)
-            finishingPoint = DFS(boardInfo, new Node(currentPos, null, false), goals[0]);
+        // Select the closest valid goal
+        CellInfo goal = goals.FirstOrDefault(g => g.Walkable);
+        if (goal == null) return Locomotion.MoveDirection.None;
 
-        else if (!goals[0].Walkable || !currentPos.Walkable || finishingPoint == null)
-        {
-            UnityEngine.Debug.Log("Meta en obstáculo");
-            return Locomotion.MoveDirection.None;
-        }
-        else
-        {
-            finishingPoint = PathNodes[countNodes].info;
-            countNodes++;
-        }
+        // Find path using A* algorithm
+        List<CellInfo> path = FindPath(boardInfo, currentPos, goal);
+        if (path == null || path.Count < 2) return Locomotion.MoveDirection.None;
 
-        if (finishingPoint.RowId < currentPos.RowId) return Locomotion.MoveDirection.Down;
-        else if (finishingPoint.RowId > currentPos.RowId) return Locomotion.MoveDirection.Up;
-        else if (finishingPoint.ColumnId < currentPos.ColumnId) return Locomotion.MoveDirection.Left;
+        // Determine direction from current position to the next step
+        CellInfo nextStep = path[1];
+        if (nextStep.RowId < currentPos.RowId) return Locomotion.MoveDirection.Down;
+        if (nextStep.RowId > currentPos.RowId) return Locomotion.MoveDirection.Up;
+        if (nextStep.ColumnId < currentPos.ColumnId) return Locomotion.MoveDirection.Left;
 
         return Locomotion.MoveDirection.Right;
     }
 
-    private CellInfo DFS(BoardInfo boardInfo, Node currentNode, CellInfo goal)
+    private List<CellInfo> FindPath(BoardInfo boardInfo, CellInfo start, CellInfo goal)
     {
-        PathNodes.Add(currentNode);
-        currentNode.visited = true;
+        List<Node> openList = new List<Node>();
+        HashSet<CellInfo> closedList = new HashSet<CellInfo>();
 
-        int prohibitedAdjacentNodes = 0;
+        Node startNode = new Node(start, null, 0, CalculateHCost(start, goal));
+        openList.Add(startNode);
 
-        CellInfo[] neighbours = currentNode.info.WalkableNeighbours(boardInfo);
-
-        foreach (CellInfo neighbor in neighbours)
+        while (openList.Count > 0)
         {
-            if (neighbor == goal)
+            // Get the node with the lowest F cost
+            Node currentNode = openList.OrderBy(node => node.FCost).ThenBy(node => node.HCost).First();
+            openList.Remove(currentNode);
+            closedList.Add(currentNode.Info);
+
+            // Check if we've reached the goal
+            if (currentNode.Info == goal)
+                return ReconstructPath(currentNode);
+
+            // Process neighbors
+            foreach (CellInfo neighbor in currentNode.Info.WalkableNeighbours(boardInfo))
             {
-                PathNodes.Add(new Node(neighbor, currentNode, false));
-                foundGoal = true;
-                break;
+                if (closedList.Contains(neighbor)) continue;
+
+                int tentativeGCost = currentNode.GCost + 1; // Assuming uniform cost for moving to adjacent tiles
+                Node neighborNode = openList.FirstOrDefault(node => node.Info == neighbor);
+
+                if (neighborNode == null)
+                {
+                    // Add new node to open list
+                    neighborNode = new Node(neighbor, currentNode, tentativeGCost, CalculateHCost(neighbor, goal));
+                    openList.Add(neighborNode);
+                }
+                else if (tentativeGCost < neighborNode.GCost)
+                {
+                    // Update G cost and parent for an existing node
+                    neighborNode.GCost = tentativeGCost;
+                    neighborNode.Parent = currentNode;
+                }
             }
         }
-        foreach (CellInfo neighbor in neighbours)
+
+        // No path found
+        return null;
+    }
+
+    private List<CellInfo> ReconstructPath(Node endNode)
+    {
+        List<CellInfo> path = new List<CellInfo>();
+        Node currentNode = endNode;
+
+        while (currentNode != null)
         {
-            if (foundGoal) break;
-
-            if ((neighbor != null) && (!PathNodes.Any(node => node.info.CellId == neighbor.CellId && node.visited)))
-                DFS(boardInfo, new Node(neighbor, currentNode, false), goal);
-
-            else prohibitedAdjacentNodes++;
-
-            if (prohibitedAdjacentNodes == 4 && currentNode.parent != null)
-                DFS(boardInfo, new Node(currentNode.parent.info, currentNode.parent.parent, false), goal);
-            else if (prohibitedAdjacentNodes == 4 && currentNode.parent == null)
-                return null;
+            path.Add(currentNode.Info);
+            currentNode = currentNode.Parent;
         }
-        return PathNodes[1].info;
+
+        path.Reverse();
+        return path;
+    }
+
+    private int CalculateHCost(CellInfo currentCell, CellInfo goal)
+    {
+        // Manhattan distance heuristic
+        return Mathf.Abs(currentCell.RowId - goal.RowId) + Mathf.Abs(currentCell.ColumnId - goal.ColumnId);
     }
 }
-
