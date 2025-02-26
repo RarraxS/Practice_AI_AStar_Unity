05e399c93b5656867352ee3321a82d91ff4a0950
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-02-26 19:32:55 +0100

    Update Node.cs

--- Assets/Rarra's Test/Node.cs
+++ Assets/Rarra's Test/Node.cs
@@ -2,12 +2,12 @@ using Assets.Scripts.DataStructures;
 
 public class Node
 {
-    public int g;
-    public int h;
-    public int f;
+    public float g;
+    public float h;
+    public float f;
     public CellInfo cell;
     public Node parent;
-    public Node(int _g, int _h, CellInfo _cell, Node _parent)
+    public Node(float _g, float _h, CellInfo _cell, Node _parent)
     {
         this.g = _g;
         this.h = _h;
